4ef3b5771975c226bf2a7e80fdb3258aef440ed1
Tomoaki Yahagi <yahagi@fact-real.com>
2015-07-24 12:08:05 +0900

    add title tag

--- lib/prunus/document/head.rb
+++ lib/prunus/document/head.rb
@@ -1,8 +1,14 @@
 class Prunus::Document::Head < Nokogiri::XML::Node
   def initialize(name, document)
+    @title = Nokogiri::XML::Node.new "title", document
+    add_child @title
   end
 
   def self.new(document)
     super("head", document)
+  end
+
+  def title=(s)
+    @title.content = s
   end
 end
