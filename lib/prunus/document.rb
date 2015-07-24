2097cbf25581f4ff13f3565020ca88ee8a31ba41
Tomoaki Yahagi <yahagi@fact-real.com>
2015-07-24 12:03:38 +0900

    html5

--- lib/prunus/document.rb
+++ lib/prunus/document.rb
@@ -5,13 +5,15 @@ class Prunus::Document < Nokogiri::HTML::Document
   attr_reader :html, :body, :head
 
   def initialize
-    super
-
     @html = Nokogiri::XML::Node.new "html", self
     self.root = @html
     @head = Head.new self
     @body = Body.new self
     html.add_child @head
     html.add_child @body
+  end
+
+  def self.new
+    parse("<!DOCTYPE html>")
   end
 end
